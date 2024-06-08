ffa73dfc620b9ed3e2bf4b49a045b856a04ac44b
Mike Pustovalov <pustovalov.mike@yandex.ru>
2024-06-08 19:09:06 +0300

    manjaro: + zellij + wezterm

---
+++ terminal/install-0014-zellij.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# https://zellij.dev/screencasts/
+
+yay -S --sudoloop --noconfirm zellij
---
+++ terminal/install-0015-wezterm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# https://wezfurlong.org/wezterm/
+
+yay -S --sudoloop --noconfirm wezterm
