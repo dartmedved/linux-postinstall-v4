779e048a41c17cda295c5c7f69cc8c2be8e28449
Mike Pustovalov <pustovalov.mike@yandex.ru>
2024-06-08 16:46:47 +0300

    manjaro: * update shells

--- sdk/install-0004-sdkman.sh
+++ sdk/install-0004-sdkman.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 curl -s "https://get.sdkman.io" | bash
+# shellcheck disable=SC2039
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk version
--- sdk/update-sdk.sh
+++ sdk/update-sdk.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+# shellcheck disable=SC2039
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk selfupdate
 sdk upgrade
